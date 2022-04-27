048fa7319d6347cd283829c6784b90997ad9ee08
WendyFell <90659020+WendyFell@users.noreply.github.com>
2022-04-27 15:14:32 -0600

    Update tic-tac-toe-game.py

--- tic-tac-toe-game.py
+++ tic-tac-toe-game.py
@@ -1,6 +1,6 @@
 """
 Tic-Tac_Toe game
-Author: Wendy Fellows
+Author: Wendy Fellows using code from pencilprogramming.com
 Your program must also meet the following requirements.
 The program must have a comment with assignment and author names.
 The program must have at least one if/then block.
@@ -12,19 +12,14 @@ board = ['1','2','3','4','5','6','7','8','9']
 empty = [0,1,2,3,4,5,6,7,8]
 
 def main():
-    
-    player = 0
-    while empty and check_win():    
-        create_board()
-        player_input(player)
-        player = int(not player)
-    if not empty:
-        print("NO WINNER!")
-
-# What functions do I need:
-# Function to replace a number with an x or an o
-# Function to make the board
-# Function to check if win or draw
+    startGame()
+    # player = 0
+    # while empty and check_win():    
+    #     create_board()
+    #     player_input(player)
+    #     player = int(not player)
+    # if not empty:
+    #     print("NO WINNER!")
 
 def create_board():
     """Make a tic tac toe board with each field using a number for the players to fill in with x or o
@@ -38,23 +33,24 @@ def create_board():
     print(f"{board[6]} | {board[7]} | {board[8]}")
 
 def player_input(player):
-    """Function to ask player to enter a number on the board to enter their X or O according to which player they are.
+    """Function to ask player to enter a number on the board to enter their X or O according to which player they are. Then puts the X or O into the correct spot on the board.
+    Parameters: player
     
     """
     player_symbol = ['X','O']
     correct_input = True
 
-    position = int(input('player {playerNo} chance! Choose field to fill {symbol} '.format(playerNo = player +1, symbol = player_symbol[player])))
+    position = int(input('player {playerNo}s turn. Choose spot to fill {symbol}: '.format(playerNo = player +1, symbol = player_symbol[player])))
 
-    if board[position] == 'X' or board[position] == 'O':
+    if board[position-1] == 'X' or board[position-1] == 'O':
         correct_input = False
 
     if not correct_input:
         print("Position already equipped")
         player_input(player)
     else:
-        empty.remove(position)
-        board[position] = player_symbol[player] 
+        empty.remove(position-1)
+        board[position-1] = player_symbol[player] 
         return 1
 
 #function checks if any player won
@@ -80,11 +76,30 @@ def check_win():
                 break
         else:
             won = False
-
     if won:
         return 0
     else:
         return 1
+
+def restart():
+    user_input = input("Would you like to play again? Type 'Yes' or 'No' ")
+
+    if user_input.lower() == "no": # Converts input to lowercase for comparison
+        return False
+    elif user_input.lower() == "yes":
+        return True
+
+def startGame():
+    while True:
+        player = 0
+        while empty and check_win():    
+            create_board()
+            player_input(player)
+            player = int(not player)
+        if not empty:
+            print("NO WINNER!")
+            if not restart():
+                return
 
 
 
