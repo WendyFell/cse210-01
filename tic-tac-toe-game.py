8d7cca080c636d3dfaecdcaabf42289e65109383
WendyFell <90659020+WendyFell@users.noreply.github.com>
2022-04-27 19:24:18 -0600

    Final changes to game
    Figured out the code to ask users to play again.

--- tic-tac-toe-game.py
+++ tic-tac-toe-game.py
@@ -1,6 +1,6 @@
 """
 Tic-Tac_Toe game
-Author: Wendy Fellows using code from pencilprogramming.com
+Author: Wendy Fellows referencing code from pencilprogramming.com
 Your program must also meet the following requirements.
 The program must have a comment with assignment and author names.
 The program must have at least one if/then block.
@@ -8,24 +8,43 @@ The program must have at least one while loop.
 The program must have more than one function.
 The program must have a function called main.
 """
-board = ['1','2','3','4','5','6','7','8','9']
-empty = [0,1,2,3,4,5,6,7,8]
+
+
+
+
 
 def main():
-    
-    player = 0
-    while empty and check_win():    
-        create_board()
-        player_input(player)
-        player = int(not player)
-    if not empty:
-        print("NO WINNER!")
+    print()
+    print("Let's play Tic-Tac-Toe!")
+   
+    playgame = True
+    while playgame == True:
+        board = ['1','2','3','4','5','6','7','8','9']
+        empty = [0,1,2,3,4,5,6,7,8]
+        player = 0
+        
+        while empty and check_win(board):    
+            create_board(board)
+            player_input(player, board, empty)
+            player = int(not player)
+        if not empty:
+            print("NO WINNER!")
 
-def create_board():
+        yes_no = play_again()
+        
+        if yes_no == "no":
+            playgame = False
+            print("Thank you for playing today!")
+            break
+      
+       
+
+
+def create_board(board):
     """Make a tic tac toe board with each field using a number for the players to fill in with x or o
     Parameter: none
     Return nothing
-    """   
+    """
     print()
     print(f"{board[0]} | {board[1]} | {board[2]}")
     print("--+---+--")
@@ -33,7 +52,7 @@ def create_board():
     print("--+---+--")
     print(f"{board[6]} | {board[7]} | {board[8]}")
 
-def player_input(player):
+def player_input(player, board, empty):
     """Function to ask player to enter a number on the board to enter their X or O according to which player they are. Then puts the X or O into the correct spot on the board.
     Parameters: player
     
@@ -41,26 +60,33 @@ def player_input(player):
     player_symbol = ['X','O']
     correct_input = True
 
-    position = int(input('player {playerNo}s turn. Choose spot to fill {symbol}: '.format(playerNo = player +1, symbol = player_symbol[player])))
+    print()
+    position = int(input("player {playerNo}'s turn. Choose a numbered spot to fill an {symbol}: ".format(playerNo = player +1, symbol = player_symbol[player])))
 
     if board[position-1] == 'X' or board[position-1] == 'O':
         correct_input = False
 
     if not correct_input:
-        print("Position already equipped")
+        print()
+        print("That spot has been used, try again.")
+        print()
         player_input(player)
     else:
         empty.remove(position-1)
         board[position-1] = player_symbol[player] 
         return 1
 
-#function checks if any player won
-def check_win():
-    #define players symbols and winning positions
+
+def check_win(board):
+    """Looks through board to see if any matching X's or O's are in a certain winning pattern.
+    Parameters: none
+
+    """
+    # Define players symbols and winning positions
     player_symbol = ['X','O']
     winning_positions =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
-    #check all winning positions for matching placements
+    # Check all winning positions for matching placements
     for check in winning_positions:
         first_symbol = board[check[0]]
         if first_symbol != ' ':
@@ -71,11 +97,14 @@ def check_win():
                     break
             if won:
                 if first_symbol == player_symbol[0]:
-                    create_board()
-                    print('player 1 won')
+                    create_board(board)
+                    print("Player 1 is the Winner!")
+                    print()
+                   
                 else:
-                    create_board()
-                    print('player 2 won')
+                    create_board(board)
+                    print("Player 2 is the Winner!")
+                    print()
                 break
         else:
             won = False
@@ -84,26 +113,12 @@ def check_win():
     else:
         return 1
 
-def restart():
-    user_input = input("Would you like to play again? Type 'Yes' or 'No' ")
-
-    if user_input.lower() == "no": # Converts input to lowercase for comparison
-        return False
-    elif user_input.lower() == "yes":
-        return True
-
-def startGame():
-    while True:
-        player = 0
-        while empty and check_win():    
-            create_board()
-            player_input(player)
-            player = int(not player)
-        if not empty:
-            print("NO WINNER!")
-            if not restart():
-                return
-
+def play_again():
+    """Function to ask if players would like to play again.
+    """
+    user_input = input("Would you like to play again? Type 'yes' or 'no': ")
+    print()
+    return user_input.lower
 
 
 if __name__ == "__main__":
